d938d18b1bda6df2fee738864d9d072f3253d3e2
saguat <167339437+saguat@users.noreply.github.com>
2024-10-27 11:46:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const pokemonWeight = document.getElementById("weight");
 const pokemonHeight = document.getElementById("height");
 const pokemonImg = document.getElementById("pokemon-img");
 const pokemonType = document.getElementById("types");
+const pokemonHp = document.getElementById("hp");
+const pokemonAttack = document.getElementById("attack");
+const pokemonDefense = document.getElementById("defense");
+const pokemonSpecialAttack = document.getElementById("special-attack");
+const pokemonSpecialDefense = document.getElementById("special-defense");
+const pokemonSpeed = document.getElementById("speed");
 
 const fetchAllPokemon = async () => {
   try {
@@ -47,22 +53,41 @@ const searchPokemon = async (inputValue) => {
 
   const specificPokemon = await fetchOnePokemon(pokemon.url);
 
+  resetPokemon();
   updatePokemon(specificPokemon); 
 }
 
 const updatePokemon = (pokemon) => {
-  
-
   pokemonName.textContent = `${pokemon.name.toUpperCase()}`;
   pokemonId.textContent = `#${pokemon.id}`;
   pokemonWeight.textContent = `Weight: ${pokemon.weight}`;
   pokemonHeight.textContent = `height: ${pokemon.height}`;
-  pokemonImg.innerHTML = `<img src="${pokemon.sprites.front_default}" alt="image of ${pokemon.name}">`;
+  pokemonImg.innerHTML = `<img src="${pokemon.sprites.front_default}" alt="image of ${pokemon.name}" id="sprite">`;
 
   const pokemonTypes = pokemon.types;
   pokemonTypes.forEach((el) => pokemonType.innerHTML += `<p class="type ${el.type.name}">${el.type.name.toUpperCase()}</p>`);
-  
-  
+
+  pokemonHp.textContent = `${pokemon.stats[0].base_stat}`;
+  pokemonAttack.textContent = `${pokemon.stats[1].base_stat}`;
+  pokemonDefense.textContent = `${pokemon.stats[2].base_stat}`;
+  pokemonSpecialAttack.textContent = `${pokemon.stats[3].base_stat}`;
+  pokemonSpecialDefense.textContent = `${pokemon.stats[4].base_stat}`;
+  pokemonSpeed.textContent = `${pokemon.stats[5].base_stat}`;
+}
+
+const resetPokemon = () => {
+  pokemonName.textContent = ``;
+  pokemonId.textContent = ``;
+  pokemonWeight.textContent = ``;
+  pokemonHeight.textContent = ``;
+  pokemonImg.innerHTML = ``;
+  pokemonType.innerHTML = ``;
+  pokemonHp.textContent = ``;
+  pokemonAttack.textContent = ``;
+  pokemonDefense.textContent = ``;
+  pokemonSpecialAttack.textContent = ``;
+  pokemonSpecialDefense.textContent = ``;
+  pokemonSpeed.textContent = ``;
 }
 
 const eventFunction = () => {
@@ -71,3 +96,9 @@ const eventFunction = () => {
 }
 
 searchBtn.addEventListener("click", eventFunction);
+
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    eventFunction();
+  }
+});
